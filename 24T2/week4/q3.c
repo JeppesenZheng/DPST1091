ec920ea4888cd7a3ed158a2124f09f3690a6d00d
Siyuan Zheng <z5449047@ad.unsw.edu.au>
2024-05-28 23:23:26 +1000

    Added scanf's return type in q3.c

--- 24T2/week4/q3.c
+++ 24T2/week4/q3.c
@@ -4,6 +4,8 @@
 // Discuss how we could create a loop that would continually read values in 
 // from standard input until the user inputs anything other than a number (or the input ends).
 
+// scanf will return 0, 1 or EOF
+
 #include <stdio.h>
 
 int main(void) {
