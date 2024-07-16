986fab2aea419eee9bf35a2dd82822f0bba1d5bd
Siyuan Zheng <z5449047@ad.unsw.edu.au>
2024-07-16 11:49:03 +1000

    Finished all tuts

--- 24T2/week11/tut11.c
+++ 24T2/week11/tut11.c
@@ -42,17 +42,47 @@ struct node *appendNode(struct node *head, int new_data) {
     return head;
 }
 
+int check_evens(struct node *head) {
+    int is_evens = 1;
+    struct node *current = head;
+    while(current != NULL) {
+        if (current->data % 2 != 0) {
+            is_evens = 0;
+        }
+        current = current->next;
+    }
+
+    return is_evens;
+}
 
 int list_evens(struct node *head1, struct node *head2) {
-	return 0;
+	if (check_evens(head1) && check_evens(head2)) {
+        return -1;
+    } else if (check_evens(head1) || check_evens(head2)) {
+        return 1;
+    } else {
+        return 0;
+    }
 }
 
 struct node *list_ordered_insert(struct node *head, int data) {
-	return NULL;
-}
+    struct node *new = create_node(data);
+	// 1. insert at the head
+    if (head->data >= data) {
+        new->next = head;
+        head = new;
+        return head;
+    }
 
-struct node *list_delete_smallest(struct node *head) {
-	return NULL;
+    struct node *prev = head;
+    while(prev->next->data < data) {
+        prev = prev->next;
+    }
+
+    new->next = prev->next;
+    prev->next = new;
+
+    return head;
 }
 
 struct node *list_copy(struct node *head1) {
@@ -61,7 +91,13 @@ struct node *list_copy(struct node *head1) {
 
 // append list2 to list1 => list1->list2
 struct node *list_append(struct node *head1, struct node *head2) {
-	return NULL;
+	// find the last node
+    struct node *last = head1;
+    while(last->next != NULL) {
+        last = last->next;
+    }
+    last->next = head2;
+    return head1;
 }
 
 // reverse the linked list
@@ -90,15 +126,59 @@ int list_count_occurrences(struct node *head, int data) {
 }
 
 struct node *removeHead(struct node *head) {
-    return NULL;
+    // 1. create temp pointer points the node we want to remove
+    struct node *temp = head;
+    // 2. move head pointer to the second node
+    head = head->next;
+    // 3. free the temp
+    free(temp);
+
+    return head;
 }
 
 struct node *removeInside(struct node *head, int target) {
-    return NULL;
+    // 1. find the previous node
+    struct node *prev = head;
+    while(prev->next->data != target) {
+        prev = prev->next;
+    }
+    // 2. create the temp pointer
+    struct node *temp = prev->next;
+    // 3. skip the target node
+    prev->next = prev->next->next;
+    // 4. free the node
+    free(temp);
+
+    return head;
 }
 
 struct node *removeTail(struct node *head) {
-    return NULL;
+    // 1. find the last two node
+    struct node *prev = head;
+    while(prev->next->next != NULL) {
+        prev = prev->next;
+    }
+    // 2. create the temp pointer
+    struct node *temp = prev->next;
+    // 3. redirect the prev->next to NULL
+    prev->next = NULL;
+    // 4. free the temp
+    free(temp);
+
+    return head;
+}
+
+struct node *list_delete_smallest(struct node *head) {
+    int smallest = head->data;
+    struct node *current = head;
+    while(current != NULL) {
+        if (current->data < smallest) {
+            smallest = current->data;
+        }
+        current = current->next;
+    }
+
+    return removeInside(head, smallest);
 }
 
 void print_list(struct node *head) {
@@ -124,6 +204,9 @@ int main() {
     head = appendNode(head, 5);
     head = appendNode(head, 6);
 
+    head = list_ordered_insert(head, 0);
+    print_list(head);
+
     struct node *head1 = NULL;
     // create second linked list like this
     // 1->3->4->5->6->9
@@ -134,6 +217,8 @@ int main() {
     head1 = appendNode(head1, 10);
     head1 = appendNode(head1, 12);
 
+    printf("the result is %d\n", list_evens(head, head));
+
     struct node *head2 = NULL;
     // create second linked list like this
     // 4->5->1->8->10->12
@@ -143,5 +228,12 @@ int main() {
     head2 = appendNode(head2, 8);
     head2 = appendNode(head2, 10);
     head2 = appendNode(head2, 12);
+
+    print_list(head2);
+    head2 = list_delete_smallest(head2);
+    print_list(head2);
+
+    head1 = list_append(head1, head2);
+    print_list(head1);
 
 }
